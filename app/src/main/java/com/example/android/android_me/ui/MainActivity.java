a80e93f258b22cb026cb4a4352331dda58028078
jgroman <jgroman@users.noreply.github.com>
2018-04-10 15:04:12 +0200

    fix: fixed empty screen on app start

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,11 +12,11 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         Log.d(TAG, "onCreate: ");
         
-        setContentView(R.layout.activity_android_me);
+        setContentView(R.layout.activity_main);
     }
 }
